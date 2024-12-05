459d5ddae0ca42a1a6b1fb69bb4f84b4143a62e5
AxelBone <axel@MACWILF-2.lan>
2024-12-05 14:02:37 +0100

    Update scripts

--- scripts/download_species.sh
+++ scripts/download_species.sh
@@ -1,39 +1,40 @@
+#!/bin/bash
+
 ###################
 
 # Téléchargement des espèces de l'étude
 
 ####################
 
-# 1) Rhincodon typus
-wget -O data/raw_data/RhinTyp.gbff.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/021/869/965/GCF_021869965.1_sRhiTyp1.1/GCF_021869965.1_sRhiTyp1.1_genomic.gbff.gz
+ENV_PATH="/home/genouest/tp_emp_tps_40958/tp59985/EMP/TP5-8/"
 
-# 2) Cetorhinus maximus
-wget -O data/raw_data/CetMax.gbff.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/964/194/155/GCA_964194155.1_sCetMax3.hap1.1/GCA_964194155.1_sCetMax3.hap1.1_genomic.gbff.gz
+. /local/env/envconda.sh
 
-# 3) Pristiophorus japonicus
-wget -O data/raw_data/CetMax.gbff.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/964/194/155/GCA_964194155.1_sCetMax3.hap1.1/GCA_964194155.1_sCetMax3.hap1.1_genomic.gbff.gz
+conda activate "$ENV_PATH/EMP_env"
 
-# 4) Heptranchias perlo
-wget -O data/raw_data/HepPer.gbff.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/035/084/215/GCF_035084215.1_sHepPer1.hap1/GCF_035084215.1_sHepPer1.hap1_genomic.gbff.gz
+#CetMax -x
+datasets download genome accession GCA_964194155.1 --include gbff --filename data/raw_data/CetMax.zip
 
-# 5) Sphyrna mokarran
-wget -O data/raw_data/SphMok.gbff.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/024/679/065/GCA_024679065.1_ASM2467906v1/GCA_024679065.1_ASM2467906v1_genomic.gbff.gz
+#HepPerl-x
+datasets download genome accession GCF_035084215.1 --include gbff --filename data/raw_data/HepPer.zip
 
-# 6) Carcharodon carcharias
-wget -O data/raw_data/CarCar.gbff.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/017/639/515/GCF_017639515.1_sCarCar2.pri/GCF_017639515.1_sCarCar2.pri_genomic.gbff.gz
+#SphMok -x
+datasets download genome accession GCA_024679065.1 --include gbff --filename data/raSphMok.zip
 
-# 7) Heterodontus francisci 
-wget -O data/raw_data/HetFra.gbff.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/036/365/525/GCF_036365525.1_sHetFra1.hap1/GCF_036365525.1_sHetFra1.hap1_genomic.gbff.gz
+# HetFra -x
+datasets download genome accession GCF_036365525.1 --include gbff --filename data/raw_data/HetFra.zip
 
-# 8) Squatina squatina
-wget -O data/raw_data/SqaSqa.gbff.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/031/763/465/GCA_031763465.1_ASM3176346v1/GCA_031763465.1_ASM3176346v1_genomic.gbff.gz
+#SqaSqa -x
+datasets download genome accession GCA_031763465.1 --include gbff --filename data/raw_data/SqaSqa.zip
 
-# 9) Squalus acanthias
-wget -O data/raw_data/SqaAca.gbff.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/030/390/025/GCA_030390025.1_ASM3039002v1/GCA_030390025.1_ASM3039002v1_genomic.gbff.gz
+#SqaAca -x
+datasets download genome accession GCA_030390025.1 --include gbff --filename data/raw_data/SqaAca.zip
 
-# 10) Latimeria chalumnae
-wget -O data/raw_data/LatCha ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/037/176/945/GCF_037176945.1_fLatCha1.pri/GCF_037176945.1_fLatCha1.pri_genomic.gbff.gz
+#CarCar - x
+datasets download genome accession GCA_024679065.1 --include gbff --filename data/raw_data/CarCar.zip
 
-# 11) Petromyzon marinus
-wget -O data/raw_data/PetMar ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/010/993/605/GCF_010993605.1_kPetMar1.pri/GCF_010993605.1_kPetMar1.pri_genomic.gbff.gz
+#LatCha -x
+datasets download genome accession GCF_037176945.1 --include gbff --filename data/raw_data/LatCha.zip
+
+
 
--- scripts/from_download_to_subset.sh
+++ scripts/from_download_to_subset.sh
@@ -9,59 +9,56 @@
 # Il faudra subset les fichiers
 # Lancer orthofinder
 
+. /local/env/envconda.sh
+
 conda activate /home/genouest/tp_emp_tps_40958/tp59985/EMP/TP5-8/EMP_env || {
     echo "Erreur: Impossible d'activer l'environnement Conda. Assurez-vous que le chemin est correct."
     exit 1
 }
 
 # Définir les répertoires source et destination
+WD="/home/genouest/tp_emp_tps_40958/tp59985/EMP/TP5-8"
 SOURCE_DIR="/home/genouest/tp_emp_tps_40958/tp59985/EMP/TP5-8/data/raw_data"
 DEST_DIR_DEZIP="/home/genouest/tp_emp_tps_40958/tp59985/EMP/TP5-8/data/raw_data/dezip"
 DEST_DIR_PROTEINS="/home/genouest/tp_emp_tps_40958/tp59985/EMP/TP5-8/data/raw_data/proteomes"
 DEST_DIR_SUBSET="/home/genouest/tp_emp_tps_40958/tp59985/EMP/TP5-8/data/raw_data/proteomes_subset"
 DEST_DIR_ORTHO="/scratch/tp59985"
 
-OUTPUT_LOG="$WD/decompression_output.log"
-ERROR_LOG="$WD/decompression_error.log"
-
-# Créer des fichiers logs
-> "$OUTPUT_LOG"
-> "$ERROR_LOG"
 
 # Vérifier si le répertoire de destination existe, sinon le créer
 if [ ! -d "$DEST_DIR_DEZIP" ]; then
     mkdir -p "$DEST_DIR_DEZIP"
 fi
 
-echo "Début du pipeline de traitement ..." | tee -a "$OUTPUT_LOG"
+echo "Début du pipeline de traitement ..." 
 
 # Etape 1 : Décompressez les fichiers .gbff.gz
-echo "Décompression des fichiers..." | tee -a "$OUTPUT_LOG"
+echo "Décompression des fichiers..." 
 
-for file in "$SOURCE_DIR"/*.gbff.gz; do
-    if [ -f "$file" ]; then
-        # Décompresser chaque fichier dans le répertoire de destination
-        gunzip -c "$file" > "$DEST_DIR_DEZIP/$(basename "${file%.gz}")"
-        echo "Décompressé : $file -> $DEST_DIR_DEZIP/$(basename "${file%.gz}")"
-    fi
-done
+# for file in "$SOURCE_DIR"/*.gbff.gz; do
+#     if [ -f "$file" ]; then
+#         # Décompresser chaque fichier dans le répertoire de destination
+#         gunzip -c "$file" > "$DEST_DIR_DEZIP/$(basename "${file%.gz}")"
+#         echo "Décompressé : $file -> $DEST_DIR_DEZIP/$(basename "${file%.gz}")"
+#     fi
+# done
 
-echo "Tous les fichiers ont été décompressés." | tee -a "$OUTPUT_LOG"
+echo "Tous les fichiers ont été décompressés."
 
 # Etape 2 : extraction des CDS et traduction en protéines
-echo "Extraction des CDS et traduction..." | tee -a "$OUTPUT_LOG"
+echo "Extraction des CDS et traduction..."
 
 for gbff_file in "$DEST_DIR_DEZIP"/*.gbff; do
     if [ -f "$gbff_file" ]; then
-        output_protein_file="$OUTPUT_DIR_PROTEINS/$(basename "${gbff_file%.gbff}.fa")"
+        output_protein_file="$DEST_DIR_PROTEINS/$(basename "${gbff_file%.gbff}.fa")"
         
-        if python3 extract_cds_translation.py "$gbff_file" "$output_protein_file"; then
-            echo "Extraction réussie : $gbff_file -> $output_protein_file" | tee -a "$OUTPUT_LOG"
+        if python3 $WD/scripts_exec/extract_cds_translation.py "$gbff_file" "$output_protein_file"; then
+            echo "Extraction réussie : $gbff_file -> $output_protein_file"
         else
-            echo "Erreur lors de l'extraction pour : $gbff_file" | tee -a "$ERROR_LOG"
+            echo "Erreur lors de l'extraction pour : $gbff_file"
         fi
     else
-        echo "Aucun fichier .gbff trouvé dans $DEST_DIR_DEZIP." | tee -a "$ERROR_LOG"
+        echo "Aucun fichier .gbff trouvé dans $DEST_DIR_DEZIP."
     fi
 done            
 
@@ -69,7 +66,7 @@ done
 # Etape 3 : Subset des protéines 
 # On va prendre 15 % du protéome 
 
-echo "Sous-échantillonnage des protéines à hauteur de 10 % du protéome ..." | tee -a "$OUTPUT_LOG"
+echo "Sous-échantillonnage des protéines à hauteur de 10 % du protéome ..."
 
 for protein_file in "$DEST_DIR_PROTEINS"/*.fa; do
     if [ -f "$protein_file" ]; then
@@ -83,22 +80,22 @@ for protein_file in "$DEST_DIR_PROTEINS"/*.fa; do
         cat "$protein_file" | awk '/^>/ {printf("\n%s\n",$0); next;} {printf("%s",$0);} END {printf("\n");}' | tail -n +2 \
             | paste - - | sort -R | head -n "$subset_size" | tr "\t" "\n" > "$output_subset_file"
         
-        echo "Sous-échantillonnage terminé : $protein_file -> $output_subset_file ($subset_size séquences)" | tee -a "$OUTPUT_LOG"
+        echo "Sous-échantillonnage terminé : $protein_file -> $output_subset_file ($subset_size séquences)"
     else
-        echo "Aucun fichier de protéines trouvé dans $DEST_DIR_PROTEINS." | tee -a "$ERROR_LOG"
+        echo "Aucun fichier de protéines trouvé dans $DEST_DIR_PROTEINS."
     fi
 done
 
 
 # ETAPE 4 : Orthofinder
 
-echo "Orthofinder avec les données subset ..." | tee -a "$OUTPUT_LOG"
+# echo "Orthofinder avec les données subset ..."
 
-threads=20
+# threads=30
 
-orthofinder -t $threads -M dendroblast -S diamond -1 -X -o $DEST_DIR_ORTHO -f $DEST_DIR_SUBSET
+# orthofinder -t $threads -M dendroblast -S diamond -1 -X -o $DEST_DIR_ORTHO -f $DEST_DIR_SUBSET
 
-echo "Pipeline complet terminé avec succès !" | tee -a "$OUTPUT_LOG"
+# echo "Pipeline complet terminé avec succès !"
 
 
 
